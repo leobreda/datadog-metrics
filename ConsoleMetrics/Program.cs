55cff9c5a3a00971da05cebf331201510f01e617
leobreda <leobreda@yahoo.com>
2025-11-30 18:52:53 -0300

    ip

--- ConsoleMetrics/Program.cs
+++ ConsoleMetrics/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleMetrics
 
             //Apontando pro container, ao rodar no Visual Studio
             // if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-                 config.StatsdServerName = "192.168.0.24";
+                 config.StatsdServerName = "192.168.0.4";
             
 
             Console.WriteLine("Hello, World!\n");
